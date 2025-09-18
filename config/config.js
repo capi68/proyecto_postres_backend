a33ca2d7e6a6a6312d0078a8f01bfb27f304f367
yersi alviarez <yersialviarez@gmail.com>
2025-09-18 12:28:20 -0500

    inicio de backend... config.json cambió a config.js para coneccion cn .env

---
+++ config/config.js
@@ -0,0 +1,24 @@
+require("dotenv").config(); // <- importa dotenv
+
+module.exports = {
+  development: {
+    username: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    dialect: process.env.DB_DIALECT,  // postgres
+  },
+  test: {
+    username: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    dialect: process.env.DB_DIALECT,
+  },
+  production: {
+    use_env_variable: "DB_URL", // <- en producción usarás la URL completa
+    dialect: "postgres",
+  },
+};
