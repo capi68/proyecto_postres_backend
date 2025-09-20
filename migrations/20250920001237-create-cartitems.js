3049871da6b9f7033923e8c0167e4e4629bbcdac
yersi alviarez <yersialviarez@gmail.com>
2025-09-19 19:25:08 -0500

    generacion de migracion 'create-cartitems'

---
+++ migrations/20250920001237-create-cartitems.js
@@ -0,0 +1,51 @@
+'use strict';
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    await queryInterface.createTable('CartItems', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+
+      productId: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        references: { model: 'Products', key: 'id' },
+        onUpdate: 'CASCADE',
+        onDelete: 'CASCADE'
+      },
+
+      userId: {
+        type: Sequelize.INTEGER,
+        allowNull: false, // Siempre debe haber un ID/cliente para que funcione
+        references: { model: 'Users', key: 'id' },
+        onUpdate: 'CASCADE',
+        onDelete: 'CASCADE'
+      },
+      
+      quantity: {
+        type: Sequelize.INTEGER,
+        allowNull: false, 
+        defaultValue: 1
+      },
+
+      createAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+
+      updateAt: {
+        allowNull: false,
+        type:Sequelize.DATE
+      }
+    });
+  },
+
+  async down (queryInterface, Sequelize) {
+    await queryInterface.dropTable('CartItems');
+  }
+};
