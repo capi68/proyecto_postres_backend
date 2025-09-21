fec0be636564b0542a1215d0416fd45f3532ed07
yersi alviarez <yersialviarez@gmail.com>
2025-09-20 20:40:16 -0500

    creacion de carpeta routes con: cartRoutes, productRoutes, userRoutes, creacion de app.js y server.js

---
+++ app.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const bodyPrser = require("body-parser");
+const db = require("./models");
+const bodyParser = require("body-parser");
+
+const app = express();
+
+app.use(bodyParser.json());
+
+//////////////////////////////////////////////////////////////////////////////
+
+//Routes
+app.use("/products", require("./routes/productRoutes"));
+app.use("/users", require("./routes/userRoutes"));
+app.use("/cart", require("./routes/cartRoutes"));
+
+//synchronize DB on startup (only development/test)
+
+db.sequelize.sync()
+.then(() => console-log("DB synchronized"))
+.catch(err => console.error("Error al sincronizar DB", err));
+
+module.exports = app;
