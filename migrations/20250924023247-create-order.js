36769ddd61a74b5c2ce75f4cfb772eab9158f114
yersi alviarez <yersialviarez@gmail.com>
2025-09-23 21:53:24 -0500

    creacion de migracion y models order.js para generar ordenes luego de tener productos en el carrito

---
+++ migrations/20250924023247-create-order.js
@@ -0,0 +1,45 @@
+'use strict';
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('Orders', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      userId: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        references: {
+          model: "Users",
+          key: "id"
+        },
+        onUpdate: "CASCADE",
+        onDelete: "CASCADE"
+      },
+      totalPrice: {
+        type: Sequelize.DECIMAL(10, 2),
+        allowNull: false,
+        defaultValue: 0.00
+      },
+      status: {
+        type: Sequelize.STRING,
+        allowNull: false,
+        defaultValue: "pending"
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('Orders');
+  }
+};
---
+++ models/order.js
@@ -0,0 +1,33 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class Order extends Model {
+    
+    static associate(models) {
+      // an order belongs to a user
+
+      Order.belongsTo(models.User, {
+        foreignKey: "userId",
+        as: "user"
+      });
+
+      // an order have many OrderItems
+      Order.hasMany(models.OrderItem, {
+        foreignKey: "orderId",
+        as: "orderItems"
+      });
+    }
+  }
+  Order.init({
+    userId: DataTypes.INTEGER,
+    totalPrice: DataTypes.DECIMAL(10, 2),
+    status: DataTypes.STRING
+  }, {
+    sequelize,
+    modelName: 'Order',
+  });
+
+  return Order;
+};
