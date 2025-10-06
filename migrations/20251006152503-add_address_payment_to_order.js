d635ebbb985b2d403b8a04f9d664873f3ad83413
yersi alviarez <yersialviarez@gmail.com>
2025-10-06 15:33:41 -0500

    migracion realizada de address y paymentMethod en orders

--- migrations/20251006152503-add_address_payment_to_order.js
+++ migrations/20251006152503-add_address_payment_to_order.js
@@ -1,22 +1,23 @@
 'use strict';
 
+const { sequelize } = require('../models');
+
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up (queryInterface, Sequelize) {
-    /**
-     * Add altering commands here.
-     *
-     * Example:
-     * await queryInterface.createTable('users', { id: Sequelize.INTEGER });
-     */
+    await queryInterface.addColumn('Orders', 'adress', {
+      type: Sequelize.STRING,
+      allowNull: true,
+    });
+
+    await queryInterface.addColumn('Orders', 'paymentMethod', {
+      type: Sequelize.STRING,
+      allowNull: true,
+    });
   },
 
   async down (queryInterface, Sequelize) {
-    /**
-     * Add reverting commands here.
-     *
-     * Example:
-     * await queryInterface.dropTable('users');
-     */
+    await queryInterface.removeColumn('Orders', 'address');
+    await queryInterface.removeColumn('Orders', 'paymentMethod');
   }
 };
