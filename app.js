c99d58e151605d173b4eef02bb1d2f8492129031
yersi alviarez <yersialviarez@gmail.com>
2025-11-06 21:36:25 -0500

    CORS debug

--- app.js
+++ app.js
@@ -6,10 +6,10 @@ const cors = require("cors");
 const app = express();
 
 // =====================================================
-//  CORS ABIERTO TEMPORAL (para evitar bloqueos y caídas)
+// CORS 
 // =====================================================
-app.use(cors());
-app.options("*", cors());
+app.use(cors()); 
+app.options("/", cors()); 
 
 // =====================================================
 // Middlewares
@@ -24,7 +24,4 @@ app.use("/users", require("./routes/userRoutes"));
 app.use("/cart", require("./routes/cartRoutes"));
 app.use("/orders", require("./routes/orderRoutes"));
 
-// =====================================================
-// Export app
-// =====================================================
 module.exports = app;
